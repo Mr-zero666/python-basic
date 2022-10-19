5df2e05a9c09b85ecfd6a0baadc725da6f6b30a6
NanYuan-dxy <80657110+Mr-zero666@users.noreply.github.com>
2022-10-19 16:07:37 +0800

    Update DecToBin.py

--- DecToBin.py
+++ DecToBin.py
@@ -1,4 +1,16 @@
 def DecToBin( n ):
+    out = list()
+    new = ''
+    while input != 0:
+        t = input % 2
+        out.append(str(t))
+        input = int(input/2)
+        if input == 1:
+            out.append(str(input))
+            break
+    out.sort(reverse=True)
+    new = new.join(out)
+    return new
     
 
 
