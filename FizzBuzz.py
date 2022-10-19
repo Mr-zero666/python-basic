b3540c3f9cf1b2078ad1ae413f55c2703be49bb6
NanYuan-dxy <80657110+Mr-zero666@users.noreply.github.com>
2022-10-19 16:12:30 +0800

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,15 @@
-def FizzBuzz( n )
+def FizzBuzz( n ):
+    a = []
+    for i in range(1,n+1):
+        if i % 4 == 0 and i % 6 == 0:
+            a.append('FizzBuzz')
+        elif i % 4 == 0:
+            a.append('Fizz')
+        elif i % 6 == 0:
+            a.append('Buzz')
+        else:
+            a.append(i)
+    return a
 
 
 
